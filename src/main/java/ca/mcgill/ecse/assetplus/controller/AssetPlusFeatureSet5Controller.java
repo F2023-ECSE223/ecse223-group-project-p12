2ed4277525cd766be3d13c2cbec5a5ebc2e8a4a9
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2023-10-06 12:39:54 -0400

    Input validation for the delete function
    Same validation as the addImage, but without return the error value

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -24,6 +24,13 @@ public class AssetPlusFeatureSet5Controller {
   }
 
   public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
+    // Input validations
+    String err =  AssetPlusFeatureUtility.isTicketIDValid(ticketID) + 
+                  AssetPlusFeatureUtility.isStringNotEmpty(imageURL);
+    if (!err.isEmpty()) {
+      return;
+    }
+
     // Find the image with the URL and delete it
     for (TicketImage image : AssetPlusApplication.getAssetPlus().getMaintenanceTicket(ticketID).getTicketImages() ) {
       if (image.getImageURL() == imageURL) {
