7aa65ed7d7903ec89bd07f9ea8587be71974c4aa
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2023-10-06 12:31:45 -0400

    Added the Utility Class and implemented feature 5
    Feature 5 is the add/remove imageURL
    
    Added in the utility class two helper methods for input validations, one checks if the string is empty and the other if the ticketID is valid

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -1,15 +1,35 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.TicketImage;
+
 public class AssetPlusFeatureSet5Controller {
 
   public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    // Input validations
+    String err =  AssetPlusFeatureUtility.isTicketIDValid(ticketID) + 
+                  AssetPlusFeatureUtility.isStringNotEmpty(imageURL);
+    if (!err.isEmpty()) {
+      return err;
+    }
+
+    // Add image
+    try {
+      AssetPlusApplication.getAssetPlus().getMaintenanceTicket(ticketID).addTicketImage(imageURL);
+    }
+    catch (RuntimeException e) {
+      return e.getMessage();
+    }
+    return ""; // empty string means operation was successful (no error)
   }
 
   public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    // Find the image with the URL and delete it
+    for (TicketImage image : AssetPlusApplication.getAssetPlus().getMaintenanceTicket(ticketID).getTicketImages() ) {
+      if (image.getImageURL() == imageURL) {
+        image.getTicket().removeTicketImage(image);
+      }
+    }
   }
 
 }
