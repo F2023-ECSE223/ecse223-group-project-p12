42aa0cb15f1908c5c30fe80b9f58b7ceb6646f49
fathisahar <sahar.fathi@mail.mcgill.ca>
2023-10-07 11:19:07 -0400

    Feature 3, details in message
    Feature 3 implementation complete.
    
    Includes:
    - Implementation of add, update and delete a SpecificAsset
    - Implementation of following utility classes: isFloorNumberValid, isRoomNumberValid, isExistingAsset, isAssetNumberValid
    -

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet3Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet3Controller.java
@@ -1,24 +1,97 @@
 package ca.mcgill.ecse.assetplus.controller;
 
 import java.sql.Date;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.AssetType;
+import ca.mcgill.ecse.assetplus.model.SpecificAsset;
+
+/**
+ * <p>Feature 3 - Add, update, and delete asset</p>
+ * @author Sahar Fathi
+ */
 
 public class AssetPlusFeatureSet3Controller {
 
+  /**
+   * <p> Add a specific asset with a number, floor and room number, purchase date and asset type. <p>
+   * @param assetNumber the number associated to the specific asset
+   * @param floorNumber the floor number on which the specific asset is located 
+   * @param roomNumber the room number in which the specific asset is located 
+   * @param purchaseDate the date on which the specific asset was purchased
+   * @param assetTypeName the type of asset which the specific asset belongs to
+   * @return
+   */
+
   public static String addSpecificAsset(int assetNumber, int floorNumber, int roomNumber,
       Date purchaseDate, String assetTypeName) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+        //Verify that the inputs are valid.
+        String err = AssetPlusFeatureUtility.isAssetNumberValid(assetNumber) + "/n"  +
+        AssetPlusFeatureUtility.isFloorNumberValid(floorNumber) + "/n"  +
+        AssetPlusFeatureUtility.isRoomNumberValid(roomNumber) + "/n"  +
+        AssetPlusFeatureUtility.isExistingAssetType(assetTypeName);
+        if (!err.isEmpty()){
+          return err;
+        }
+
+        //Add the specific asset to the AssetPlus application instance.
+        try {
+          for (AssetType type : AssetPlusApplication.getAssetPlus().getAssetTypes()){
+            if(type.getName() == assetTypeName)
+              AssetPlusApplication.getAssetPlus().addSpecificAsset(assetNumber, floorNumber, roomNumber, purchaseDate, type);
+          }
+          
+        } catch (RuntimeException e){
+          return e.getMessage();
+        }
+        return "";
   }
 
   public static String updateSpecificAsset(int assetNumber, int newFloorNumber, int newRoomNumber,
       Date newPurchaseDate, String newAssetTypeName) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+        //Verify that the inputs are valid.
+        String err = AssetPlusFeatureUtility.isExistingAsset(assetNumber) + "/n"  +
+        AssetPlusFeatureUtility.isFloorNumberValid(newFloorNumber) + "/n"  +
+        AssetPlusFeatureUtility.isRoomNumberValid(newRoomNumber) + "/n"  +
+        AssetPlusFeatureUtility.isExistingAssetType(newAssetTypeName);
+        if (!err.isEmpty()){
+          return err;
+        }
+
+        //Update the specific asset (found based on its assetNumber) and change the fields. 
+        try {
+          for (SpecificAsset asset : AssetPlusApplication.getAssetPlus().getSpecificAssets()) {
+            if (asset.getAssetNumber() == assetNumber) {
+              asset.setFloorNumber(newFloorNumber);
+              asset.setRoomNumber(newRoomNumber);
+              asset.setPurchaseDate(newPurchaseDate);
+              for (AssetType type : AssetPlusApplication.getAssetPlus().getAssetTypes()){
+                if(type.getName() == newAssetTypeName)
+                  asset.setAssetType(type);
+              }
+            }
+          }
+       } catch (RuntimeException e){
+          return e.getMessage();
+        }
+        return "";
   }
 
   public static void deleteSpecificAsset(int assetNumber) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    //Verify that the specific asset exists.
+    String err = AssetPlusFeatureUtility.isExistingAsset(assetNumber);
+        if (!err.isEmpty()){
+          System.out.println(err);
+          return;
+        }
+        
+        //Delete the specific asset from the AssetPlus application instance. 
+        for (SpecificAsset asset : AssetPlusApplication.getAssetPlus().getSpecificAssets()) {
+          if (asset.getAssetNumber() == assetNumber) {
+            AssetPlusApplication.getAssetPlus().removeSpecificAsset(asset);
+          }
+      }
+      
+        
   }
 
 }
--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureUtility.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureUtility.java
@@ -1,7 +1,9 @@
 package ca.mcgill.ecse.assetplus.controller;
 
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.AssetPlus;
 import ca.mcgill.ecse.assetplus.model.AssetType;
+import ca.mcgill.ecse.assetplus.model.SpecificAsset;
 
 public class AssetPlusFeatureUtility {
   // Input validation static methods:
@@ -46,4 +48,41 @@ public class AssetPlusFeatureUtility {
 
     return "Error: the asset type specified with this name does not exist";
   }
+
+  public static String isFloorNumberValid(int floorNumber){
+    if (floorNumber <= 0){
+      return "Error: the floorNumber must be greater than 0";
+    }  
+
+    return "";
+  }
+
+    public static String isRoomNumberValid(int roomNumber){
+      if (roomNumber <= 0){
+        return "Error: the roomNumber must be greater than 0";
+      }  
+
+      return "";
+    }
+
+    public static String isExistingAsset(int assetNumber) {
+      for (SpecificAsset asset : AssetPlusApplication.getAssetPlus().getSpecificAssets()) {
+          if (asset.getAssetNumber() == assetNumber) {
+              return "";
+          }
+      }
+
+      return "Error: The assetNumber is not found in the list of assets.";
+    }
+
+    public static String isAssetNumberValid(int assetNumber) {
+      if (assetNumber <= 0){
+        return "Error: the assetNumber must be greater than 0";
+      }  
+
+      return "";
+    }
+
+
+
 }
