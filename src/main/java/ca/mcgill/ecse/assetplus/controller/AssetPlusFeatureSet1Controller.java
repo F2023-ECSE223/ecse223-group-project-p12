dadb9ca7cf713b2458645904898f9125158067ff
Camomille103 <camille.pouliot@mail.mcgill.ca>
2023-10-08 10:42:49 -0400

    Feature 1 and method added to utility
    Feature 1:
    -All the methods are implemented
    
    Utility:
    -a method was added to verify if the email of an employee ends with "@ap.com" as required

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
@@ -1,21 +1,88 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.User;
+
+/**
+ * <p>Feature 1 - Update manager password / add employee or guest / update employee or guest</p>
+ * @author Camille Pouliot
+ */
 public class AssetPlusFeatureSet1Controller {
 
+  /**
+   * <p>Update the password of the manager<p>
+   * @param password
+   */
   public static String updateManager(String password) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    if (password.isEmpty()) {
+      return "Error: the password cannot be empty";
+    }
+
+    try {
+      AssetPlusApplication.getAssetPlus().getManager().setPassword(password);
+    }
+    catch (RuntimeException e){
+      return e.getMessage();
+    }
+    return "";
   }
 
+  /**
+   * <p>Add an employee or a guest dependaug if the parameter isEmployee is true or false<p>
+   * @param email
+   * @param password
+   * @param name
+   * @param phoneNumber
+   * @param isEmployee
+   */
   public static String addEmployeeOrGuest(String email, String password, String name, String phoneNumber,
         boolean isEmployee) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    String err = AssetPlusFeatureUtility.isExistingUser(email);
+
+    if (err.isEmpty()) {
+      return "Error: a user with this email already exist";
+    }
+    
+    if (!AssetPlusFeatureUtility.isEmployeeEmailValid(email) && isEmployee) {
+      return "Error: the email for an employee needs to end with \"@ap.com\"";
+    }
+
+    try {
+      if (isEmployee) {
+       AssetPlusApplication.getAssetPlus().addEmployee(email, name, password, phoneNumber); 
+      } else {
+        AssetPlusApplication.getAssetPlus().addGuest(email, name, password, phoneNumber);
+      }
+    } catch (RuntimeException e) {
+      return e.getMessage();
+    }
+    
+    return "";
   }
 
+  /**
+   * <p>Update an employee or a guest with their email<p>
+   * @param email
+   * @param newPassword
+   * @param newName
+   * @param newPhoneNumber
+   */
   public static String updateEmployeeOrGuest(String email, String newPassword, String newName, String newPhoneNumber) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    String err = AssetPlusFeatureUtility.isExistingUser(email);
+
+    if (!err.isEmpty()) {
+      return err;
+    }
+
+    try {
+      User userToUpdate = User.getWithEmail(email);
+      userToUpdate.setName(newName);
+      userToUpdate.setPassword(newPassword);
+      userToUpdate.setPhoneNumber(newPhoneNumber);
+    } catch (RuntimeException e) {
+      return e.getMessage();
+    }
+    return "";
   }
 
 }
