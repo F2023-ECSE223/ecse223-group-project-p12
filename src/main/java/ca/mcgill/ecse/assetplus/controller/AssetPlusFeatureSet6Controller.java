9dfb534bada22a4a274276f4e7b7e5c03103dcce
julia-b-grenier <julia.grenier@mail.mcgill.ca>
2023-10-23 13:11:53 -0400

    Make the method isStringValid work for error message

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
@@ -22,7 +22,7 @@ public class AssetPlusFeatureSet6Controller {
    */
   public static void deleteEmployeeOrGuest(String email) {
     // Input validations
-    String err = AssetPlusFeatureUtility.isStringValid(email, "email") + 
+    String err = AssetPlusFeatureUtility.isStringValid(email, "email", "cannot") + 
                  AssetPlusFeatureUtility.isExistingUser(email);
     if (!err.isEmpty()) {
       return;
