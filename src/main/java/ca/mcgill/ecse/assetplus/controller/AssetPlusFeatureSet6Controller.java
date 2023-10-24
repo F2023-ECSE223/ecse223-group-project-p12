51c8cee1686169c50c41aa14e858d59210ccea68
Tayba <tayba.jusab25@gmail.com>
2023-10-23 20:17:25 -0400

    Add feature 7 controller methods

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
@@ -23,7 +23,7 @@ public class AssetPlusFeatureSet6Controller {
   public static void deleteEmployeeOrGuest(String email) {
     // Input validations
     String err = AssetPlusFeatureUtility.isStringValid(email, "email", "cannot") + 
-                 AssetPlusFeatureUtility.isExistingUser(email);
+                 AssetPlusFeatureUtility.isExistingUser(email, "");
     if (!err.isEmpty()) {
       return;
     }
