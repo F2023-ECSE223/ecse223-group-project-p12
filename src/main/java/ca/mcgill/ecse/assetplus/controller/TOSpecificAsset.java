01e9f0b0928fc4a9a3b616cff3bc786674f82837
fathisahar <sahar.fathi@mail.mcgill.ca>
2023-11-15 20:44:47 -0500

    to stuff

--- src/main/java/ca/mcgill/ecse/assetplus/controller/TOSpecificAsset.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/TOSpecificAsset.java
@@ -38,7 +38,107 @@ public class TOSpecificAsset {
     assetType = asset.getAssetType();
     assetPlus = asset.getAssetPlus();
   }
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TOSpecificAsset Attributes
+  private int assetNumber;
+  private int floorNumber;
+  private int roomNumber;
+  private Date purchaseDate;
+  private AssetType assetType;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TOSpecificAsset(int aAssetNumber, int aFloorNumber, int aRoomNumber, Date aPurchaseDate, AssetType aAssetType)
+  {
+    assetNumber = aAssetNumber;
+    floorNumber = aFloorNumber;
+    roomNumber = aRoomNumber;
+    purchaseDate = aPurchaseDate;
+    assetType = aAssetType;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setFloorNumber(int aFloorNumber)
+  {
+    boolean wasSet = false;
+    floorNumber = aFloorNumber;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setRoomNumber(int aRoomNumber)
+  {
+    boolean wasSet = false;
+    roomNumber = aRoomNumber;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setPurchaseDate(Date aPurchaseDate)
+  {
+    boolean wasSet = false;
+    purchaseDate = aPurchaseDate;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean setAssetType(AssetType aAssetType)
+  {
+    boolean wasSet = false;
+    assetType = aAssetType;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public int getAssetNumber()
+  {
+    return assetNumber;
+  }
+
+  public int getFloorNumber()
+  {
+    return floorNumber;
+  }
+
+  public int getRoomNumber()
+  {
+    return roomNumber;
+  }
+
+  public Date getPurchaseDate()
+  {
+    return purchaseDate;
+  }
+
+  public AssetType getAssetType()
+  {
+    return assetType;
+  }
+
+  public void delete()
+  {}
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "assetNumber" + ":" + getAssetNumber()+ "," +
+            "floorNumber" + ":" + getFloorNumber()+ "," +
+            "roomNumber" + ":" + getRoomNumber()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "purchaseDate" + "=" + (getPurchaseDate() != null ? !getPurchaseDate().equals(this)  ? getPurchaseDate().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+            "  " + "assetType" + "=" + (getAssetType() != null ? !getAssetType().equals(this)  ? getAssetType().toString().replaceAll("  ","    ") : "this" : "null");
+  }
 }
+
 
 
 
