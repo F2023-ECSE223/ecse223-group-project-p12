d3e88777b7b2f9edc5f958da267e729d6ee09209
EmiliaGagne <emiliagagne03@gmail.com>
2023-11-18 17:44:05 -0500

    Added the add asset type pop up

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AssetTypesController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AssetTypesController.java
@@ -4,13 +4,20 @@ package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class AssetTypesController {
 
@@ -28,10 +35,25 @@ public class AssetTypesController {
     private Button addAssetTypeButton;
 
     @FXML
+    private Button AddAssetTypeCreateButton;
+
+    @FXML
+    private Button AddAssetTypeCancelButton;
+
+    @FXML
     private Label replaceMe;
 
     @FXML
     private TextField ticketNumberField;
+
+    @FXML
+    private TextField AddAssetTypeName;
+
+    @FXML
+    private TextField AddAssetTypeLifespan;
+
+    @FXML 
+    private AnchorPane AddAssetTypePane;
 
     @FXML
     void AddImage(ActionEvent event) {
@@ -40,7 +62,18 @@ public class AssetTypesController {
 
     @FXML
     void addAssetTypeClicked(ActionEvent event) {
-
+      AddAssetTypePane.setVisible(true); 
     }
 
+    @FXML
+    void AddAssetTypeCreateButtonClicked(ActionEvent event) {
+      AddAssetTypePane.setVisible(false); 
+    }
+
+    @FXML
+    void AddAssetTypeCancelButtonClicked(ActionEvent event) {
+      AddAssetTypePane.setVisible(false); 
+    }
 }
+
+
