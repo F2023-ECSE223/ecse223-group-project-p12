df5e14193a4964519de7b4ce3c91c3b41b8d032a
Tayba <tayba.jusab25@gmail.com>
2023-11-19 16:38:00 -0500

    Add TableView in TicketStatus

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/TicketStatusController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/TicketStatusController.java
@@ -1,16 +1,29 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureSet6Controller;
+import ca.mcgill.ecse.assetplus.controller.TOMaintenanceTicket;
 import ca.mcgill.ecse.assetplus.javafx.fxml.AssetPlusFXMLView;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import java.util.ResourceBundle;
+import java.util.List;
+import java.text.SimpleDateFormat;;
 
 public class TicketStatusController {
+
+    @FXML
+    private ResourceBundle resources;
 
     @FXML
     private HBox DashboardAndContent;
@@ -19,22 +32,69 @@ public class TicketStatusController {
     private VBox TopContent;
 
     @FXML
-    private Button addImageButton;
+    private Button addTicketButton;
 
     @FXML
-    private Label replaceMe;
+    private ChoiceBox<String> statusChoiceBox;
 
     @FXML
     private TextField ticketNumberField;
 
     @FXML
-    void AddImage(ActionEvent event) {
+    private TableView<TOMaintenanceTicket> ticketTable;
 
-    }
+    @FXML
+    private TableColumn<TOMaintenanceTicket, Integer> ticketNumberColumn;
 
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> assetColumn;
+
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> reporterColumn;
+
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> assigneeColumn;
+
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> dateStartedColumn;
+
+    @FXML
+    private TableColumn<TOMaintenanceTicket, String> statusColumn;
+
+    @FXML
+    private TableColumn<TOMaintenanceTicket, HBox> actionColumn;
 
     @FXML
     void initialize() {
-        
+        resources = AssetPlusFXMLView.getInstance().getBundle();
+
+        statusChoiceBox.getItems().addAll(
+            resources.getString("key.Open"),
+            resources.getString("key.Assigned"),
+            resources.getString("key.InProgress"),
+            resources.getString("key.Resolved"),
+            resources.getString("key.Closed")
+        );
+
+        statusChoiceBox.setValue(resources.getString("key.SelectStatus"));
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+
+        ticketNumberColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getId()).asObject());
+        assetColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAssetName()));
+        reporterColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getRaisedByEmail()));
+        assigneeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getFixedByEmail()));
+        dateStartedColumn.setCellValueFactory(cellData -> new SimpleStringProperty(dateFormat.format(cellData.getValue().getRaisedOnDate())));
+        statusColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getStatus()));
+
+        List<TOMaintenanceTicket> tickets = AssetPlusFeatureSet6Controller.getTickets();
+        ObservableList<TOMaintenanceTicket> observableTickets = FXCollections.observableList(tickets);
+        ticketTable.setItems(observableTickets);
     }
+
+    @FXML
+    void goToTicketMenu(ActionEvent event) {
+        AssetPlusFXMLView.getInstance().changeTab("pages/TicketMenu.fxml");
+    }
+
 }
