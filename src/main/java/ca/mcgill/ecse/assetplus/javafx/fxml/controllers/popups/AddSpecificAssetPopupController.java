265debc1751ccfc50af4d7cb3e9f9954922d75f9
Anjii00 <anjalisinghal610@gmail.com>
2023-11-24 17:58:06 -0500

    small error fixed

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/popups/AddSpecificAssetPopupController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/popups/AddSpecificAssetPopupController.java
@@ -91,7 +91,7 @@ public class AddSpecificAssetPopupController {
         ViewUtils.showError(error.toString());
       } else {
         int number = (AssetPlusFeatureTOController.getSpecificAssets().get(AssetPlusFeatureTOController.getSpecificAssets().size()-1).getAssetNumber()+1);
-        AssetPlusFeatureSet3Controller.addSpecificAsset(number, Integer.parseInt(floorChoice.getValue()), Integer.parseInt(roomChoice.getValue()), java.sql.Date.valueOf(dateChoice.getValue()), assetTypes.getValue());`
+        AssetPlusFeatureSet3Controller.addSpecificAsset(number, Integer.parseInt(floorChoice.getValue()), Integer.parseInt(roomChoice.getValue()), java.sql.Date.valueOf(dateChoice.getValue()), assetTypes.getValue());
         AssetPlusFXMLView.getInstance().closePopUpWindow();
       }
     }
