94d87a20de95483c666332993b5428b9c12714c9
fathisahar <sahar.fathi@mail.mcgill.ca>
2023-11-23 23:06:46 -0500

    add functionality

--- src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddSpecificAssetPopupController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/javafx/fxml/controllers/AddSpecificAssetPopupController.java
@@ -1,8 +1,13 @@
 package ca.mcgill.ecse.assetplus.javafx.fxml.controllers;
 
 import java.nio.file.AtomicMoveNotSupportedException;
+import java.util.ArrayList;
+import ca.mcgill.ecse.assetplus.controller.AssetPlusFeatureTOController;
+import ca.mcgill.ecse.assetplus.controller.TOAssetType;
 import ca.mcgill.ecse.assetplus.javafx.fxml.AssetPlusFXMLView;
 import ca.mcgill.ecse.assetplus.javafx.fxml.controllers.ViewUtils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,6 +15,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 
 
@@ -19,7 +25,7 @@ public class AddSpecificAssetPopupController {
     private TextField assetNumber;
 
     @FXML
-    private ChoiceBox<?> assetTypes;
+    private ChoiceBox<String> assetTypes;
 
     @FXML
     private Button cancelAssetBtn;
@@ -34,16 +40,19 @@ public class AddSpecificAssetPopupController {
     private Rectangle fieldBg;
 
     @FXML
-    private ChoiceBox<?> floorChoice;
+    private ChoiceBox<String> floorChoice;
 
     @FXML
     private TextField lifeExpectancy;
 
     @FXML
-    private ChoiceBox<?> roomChoice;
+    private ChoiceBox<String> roomChoice;
 
     @FXML
     private Label topPopups;
+
+    @FXML
+    private VBox lifeExpectancyBox;
 
     @FXML
     void cancel(ActionEvent event) {
@@ -55,5 +64,55 @@ public class AddSpecificAssetPopupController {
     void create(ActionEvent event) {
 
     }
+    public void initialize() {
+
+      assetNumber.setEditable(false);
+      assetNumber.setText((AssetPlusFeatureTOController.getSpecificAssets().get(AssetPlusFeatureTOController.getSpecificAssets().size()-1).getAssetNumber()+1)+"");
+      assetNumber.setFocusTraversable(false);
+      
+      ArrayList<String> types = new ArrayList<>();
+      for (TOAssetType type : AssetPlusFeatureTOController.getAssetTypes()){
+        types.add(type.getName());
+      }
+      types.add("No asset type");
+
+      ObservableList<String> typesList = FXCollections.observableArrayList(types);
+      assetTypes.setItems(typesList);
+
+      assetTypes.setValue("Select an asset type");
+      
+      lifeExpectancyBox.setVisible(false);
+
+      ArrayList<String> rooms = new ArrayList<>();
+      for (int i = 0; i <= 50; i++) {
+        rooms.add(i+"");
+      }
+      rooms.add("No room");
+
+      ObservableList<String> roomsList = FXCollections.observableArrayList(rooms);
+      roomChoice.setItems(roomsList);
+      
+      roomChoice.setValue("Select a room");
+
+      ArrayList<String> floors = new ArrayList<>();
+      for (int i = 0; i <= 20; i++) {
+        floors.add(i+"");
+      }
+      floors.add("No floor");
+      
+      ObservableList<String> floorsList = FXCollections.observableArrayList(floors);
+      floorChoice.setItems(floorsList);
+
+      floorChoice.setValue("Select a floor");
+
+    }
+
+    @FXML
+    private void handleAssetType(ActionEvent event) {
+      String selectedValue = assetTypes.getValue();
+      System.out.println("Selected: " + selectedValue);
+      lifeExpectancyBox.setVisible(true);
+
+   }
 
 }
